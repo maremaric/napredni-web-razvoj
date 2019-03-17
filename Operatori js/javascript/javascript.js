ad5754235fc80f5326f22cf00f9289d7bc1a026e
maremaric <markoreus888@gmail.com>
2019-03-17 19:44:05 +0100

    javascript2

---
+++ Operatori js/javascript/javascript.js
@@ -0,0 +1,49 @@
+// let osoba = {
+//   ime: "Marko",
+//   prezime: "Maric",
+//   godina_rodjenja: 1988,
+//   dan: 1,
+//   mesec: 1,
+//   hobi: "Plivanje",
+//   zanimanje: "Programer"
+// };
+
+// let zbir = osoba.dan + osoba.mesec + osoba.godina_rodjenja;
+// let dani = (2019 - osoba.godina_rodjenja) * 365;
+
+// document.getElementById("zbir").innerHTML = zbir;
+// document.getElementById("dani").innerHTML = dani;
+
+// document.getElementById("ime").innerHTML = osoba.ime;
+// document.getElementById("prezime").innerHTML = osoba.prezime;
+// document.getElementById("godina_rodjenja").innerHTML = osoba.godina_rodjenja;
+// document.getElementById("hobi").innerHTML = osoba.hobi;
+// document.getElementById("zanimanje").innerHTML = osoba.zanimanje;
+
+// let zbir;
+// zbira();
+
+// function zbira() {
+//   let a = 5;
+//   let b = 10;
+//   zbir = a + b;
+
+//   zbirb();
+//   function zbirb() {
+//     zbir = a - b;
+//   }
+//   console.log(zbir);
+// }
+
+// function racunanje(a, b) {
+//   return a + b;
+// }
+// console.log("Sabiranje A i B parametra: ", racunanje(5, 5));
+
+// console.log(eval("10 + 10"));
+
+// function zbira(a, b) {
+//   d = (a + b) / 2;
+//   return d;
+// }
+// console.log(zbira(5, 5));
---
+++ js uvod/javascript.js
@@ -0,0 +1,1 @@
+document.write("Dobro dosli na sajt!");
